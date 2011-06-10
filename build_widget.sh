5858435b1ff80cbdba9f15a952153e1e75a6a950
Milen Petrinski <gonzo@greatgonzo.net>
2011-06-11 00:51:47 +0300

    Add Nokia's platformservices.js only to the Symbian WRT widget.'
    Just commented the line in index.html and added sed command to build_widget.sh to remove the comment tags.'

--- build_widget.sh
+++ build_widget.sh
@@ -6,6 +6,7 @@ zip fivecorners.wgt config.xml index.html *.js style.css icon_*.png license.txt
 # Nokia Web Runtime Widget
 mkdir fivecorners
 cp info.plist index.html icon.png *.js style.css license.txt fivecorners/
+sed -e 's/<!--BEGIN.*//g' -e 's/.*END-->//g' < index.html > fivecorners/index.html
 zip fivecorners.wgz fivecorners/*
-rm -rf fivecorners
+#rm -rf fivecorners
 
